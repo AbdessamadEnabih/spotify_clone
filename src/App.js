0c691d62608d8a9ce796383131e838b4d5cdaec0
Abdessamad Enabih <a.enabih@gmail.com>
2021-06-22 22:00:31 +0100

    Added State Management

--- src/App.js
+++ src/App.js
@@ -4,26 +4,36 @@ import Login from './components/Login'
 import { getTokenFromUrl } from './components/js/spotify'
 import SpotifyWebApi from 'spotify-web-api-js';
 import Player from './components/Player';
+import { useDataLayerValue } from './DataLayer';
+
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null)
+  
+  const [{user}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const _token = getTokenFromUrl().access_token;
 
     if(_token){ 
       setToken(_token);
-      console.log(_token);
+
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
+
       });
     }
 
-  },[]);
+  },[dispatch]);
+
+  console.log(user);
 
   return (
     <div className="app">
